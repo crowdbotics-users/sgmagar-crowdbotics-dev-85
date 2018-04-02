909c729eb0526c83082cd3031b28030d505986c2
Saurav <sp.gharti@gmail.com>
2018-04-02 18:27:30 +0000

    remove package django-paypal

--- home/views.py
+++ home/views.py
@@ -9,7 +9,6 @@ def home(request):
 	{'name':'dj-paypal', 'url': 'http://pypi.python.org/pypi/dj-paypal/0.6.0'},
 	{'name':'dj-stripe', 'url': 'http://pypi.python.org/pypi/dj-stripe/1.0.0.post1'},
 	{'name':'django-twitter', 'url': 'http://pypi.python.org/pypi/django-twitter/0.1.0'},
-	{'name':'django-paypal', 'url': 'http://pypi.python.org/pypi/django-paypal/0.4.1'},
     ]
     context = {
         'title': 'sgmagar-crowdbotics-dev-85',
